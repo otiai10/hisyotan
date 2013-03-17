12ff25eef278719b1a68c7e4581e508b04b6af65
otiai10 <otiai10@gmail.com>
2013-03-17 11:11:18 +0900

    fix bug : task ''?

--- procedure.js
+++ procedure.js
@@ -51,43 +51,45 @@ exports.switchWeeklyRemind = function(master, do_weekly, cb){
 }
 
 exports.removeTasksFromMasterName = function(master,ref_tasks,cb){
-  // TODO: ここの処理かっこわるくないか？
+
   var current_tasks  = master.tasks;
-  var new_tasks      = [];
   var done_tasks     = [];
   var not_found      = [];
-
   // done_tasksとnot_foundを決定する
   for(var i=0; i<ref_tasks.length; i++){
-    if(in_array(ref_tasks[i], current_tasks)){
-      done_tasks.push(ref_tasks[i]);
+    var match = in_array(ref_tasks[i], current_tasks);
+    if(match){
+      current_tasks.splice(match.index,1);
     }else{
       not_found.push(ref_tasks[i]);
     }
   }
 
-  // new_tasks決定する
-  for(var i=0; i<current_tasks.length; i++){
-    if(in_array(current_tasks[i], done_tasks)){
-      //console.log(current_tasks[i], 'doneなので何もしない');
-    }else{
-      //console.log(current_tasks[i], 'プッシする');
-      new_tasks.push(current_tasks[i]);
+  // ゴミを排除
+  for (var i =0; i<current_tasks.length; i++) {
+    if(current_tasks[i] == 0){
+      current_tasks.splice(i,1);
+    }
+  }
+  for (var i =0; i<current_tasks.length; i++) {
+    if(current_tasks[i] == void 0){
+      current_tasks.splice(i,1);
     }
   }
 
-  // master情報の上塗り
-  master.tasks = new_tasks;
+  master.tasks = current_tasks;
 
   mngs.saveMaster(master, function(is_success){
     if(is_success){
       var response = {
         'done_tasks'   :/*array*/done_tasks,
-        'left_tasks'   :/*array*/new_tasks,
+        'left_tasks'   :/*array*/current_tasks,
         'not_found'    :/*array*/not_found,
-        'for_example'  :/*string*/new_tasks.shift(),
+        'for_example'  :/*string*/current_tasks.shift(),
       };
       cb(response);
+    }else{
+      console.log('saveMaster がしっぱい');
     }
   });
 }
@@ -163,7 +165,7 @@ exports.extractPDFText = function(masterName, cb){
 /*private*/function in_array(target, arr){
   for(var i=0; i<arr.length; i++){
     if(arr[i] == target){
-      return arr[i];
+      return { index : i, value : arr[i]};
     }
   }
   return false;
