b670f88d8e109fb9ece399256941aac8594d3024
otiai10 <otiai10@gmail.com>
2013-06-02 15:31:28 +0900

    improve cli

--- cli/app.sh
+++ cli/app.sh
@@ -5,30 +5,34 @@ if [ $# -lt 1 ]; then
   exit 0
 fi
 
+cur_date=`date '+%Y.%m%d'`
+node_log_path='log/hisyotan.'$cur_date'.log'
+watcher_log_path='log/watcher.'$cur_date'.log'
+
 case $1 in
   "--start" )
     sleep 1s
-    nohup node $PWD/hisyo_app debug >> log/hisyotan.log &
+    nohup node $PWD/hisyo_app debug >> $node_log_path &
     echo "Hisyotan started."
     sleep 1s
-    nohup sh $PWD/cron_set/watcher.sh >> log/watcher.log &
+    nohup sh $PWD/cron_set/watcher.sh >> $watcher_log_path &
     echo "Watcher started."
     sleep 1s
     break;;
   "--stop" )
     sleep 1s
-    node $PWD/hisyo_app stop >> log/hisyotan.log &
+    node $PWD/hisyo_app stop
     echo "Hisyotan Stopped."
     sleep 1s
     break;;
   "--restart" )
-    node $PWD/hisyo_app stop >> log/hisyotan.log &
+    node $PWD/hisyo_app stop >> $node_log_path &
     echo "Hisyotan Stopped."
     sleep 1s
-    nohup node $PWD/hisyo_app debug >> log/hisyotan.log &
+    nohup node $PWD/hisyo_app debug >> $node_log_path &
     echo "Hisyotan started."
     sleep 1s
-    nohup sh $PWD/cron_set/watcher.sh >> log/watcher.log &
+    nohup sh $PWD/cron_set/watcher.sh >> $watcher_log_path &
     echo "Watcher started."
     sleep 1s
     break;;
